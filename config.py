1df0e6385161be26fa296860076cebc3e50502f8
Friendlybot666 <121375366+Friendlybot666@users.noreply.github.com>
2023-01-24 11:06:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "15109559"))
+API_ID = int(environ.get("API_ID", "15109559")
 API_HASH = environ.get("API_HASH", "75f87cab155bf35a21e831b91a4b7148")
 BOT_TOKEN = environ.get("BOT_TOKEN", "5883472079:AAEDu-jsQBttyuFeMsORA8GAq_P0O4wNiwY")
-ADMIN = int(environ.get("ADMIN", "5958870787"))          
+ADMIN = int(environ.get("ADMIN", "5958870787")   
 CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
